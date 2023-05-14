ff5f06de5bf447aaebfa2f7aac6546dc3d1ea754
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2023-05-14 20:01:23 +0300

    make message json letters case dependent on the env variable

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 
 console.log("Hello World");
 
@@ -14,6 +15,15 @@ app.use(middlewareFunction)
 
 app.get("/", (req, res)=>{
     res.sendFile(absolutePath)
+})
+
+app.get("/json", (req, res)=>{
+    let helloMessage = "Hello json"
+    let {MESSAGE_STYLE} = process.env
+    if (MESSAGE_STYLE=="uppercase") {
+        helloMessage = helloMessage.toUpperCase()
+    }
+    res.json({"message": helloMessage})
 })
 
  module.exports = app;
